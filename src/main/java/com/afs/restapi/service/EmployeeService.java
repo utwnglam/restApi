b7aee3511aaf189b2786808ca0582db1d7b1c79f
CHEUNJO2 <cheunjo2@oocl.com>
2021-12-09 17:46:06 +0800

    [rest api] [joanne] fix: change all from repository to mongodb

--- src/main/java/com/afs/restapi/service/EmployeeService.java
+++ src/main/java/com/afs/restapi/service/EmployeeService.java
@@ -1,55 +1,66 @@
 package com.afs.restapi.service;
 
 import com.afs.restapi.entity.Employee;
+import com.afs.restapi.exception.NoEmployeeFoundException;
 import com.afs.restapi.repository.EmployeeRepository;
+import com.afs.restapi.repository.EmployeeRepositoryInMongo;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class EmployeeService {
-  private EmployeeRepository employeeRepository;
+  private final EmployeeRepositoryInMongo employeeRepositoryInMongo;
 
-  public EmployeeService(EmployeeRepository employeeRepository) {
-    this.employeeRepository = employeeRepository;
+  public EmployeeService(EmployeeRepositoryInMongo employeeRepositoryInMongo) {
+    this.employeeRepositoryInMongo = employeeRepositoryInMongo;
   }
 
   public List<Employee> findAll() {
-    return employeeRepository.findAll();
+    return employeeRepositoryInMongo.findAll();
   }
 
   public Employee findById(String id) {
-    return employeeRepository.findById(id);
+    return employeeRepositoryInMongo.findById(id)
+      .orElseThrow(NoEmployeeFoundException::new);
   }
 
   public List<Employee> findByGender(String gender) {
-    return employeeRepository.findByGender(gender);
+    return employeeRepositoryInMongo.findByGender(gender);
+//    return employeeRepository.findByGender(gender);
   }
 
   public List<Employee> findByPage(Integer page, Integer pageSize) {
-    return employeeRepository.findByPageNumber(page, pageSize);
+    return employeeRepositoryInMongo.findAll(PageRequest.of(page-1, pageSize))
+      .getContent();
+//    return employeeRepository.findByPageNumber(page, pageSize);
   }
 
   public Employee create(Employee employee) {
-    return employeeRepository.create(employee);
+    return employeeRepositoryInMongo.insert(employee);
   }
 
   public Employee edit(String id, Employee updatedEmployee) {
-    Employee employee = employeeRepository.findById(id);
+    Employee employee = findById(id);
     if (updatedEmployee.getAge() != null && !updatedEmployee.getAge().equals(0)) {
       employee.setAge(updatedEmployee.getAge());
     }
     if (updatedEmployee.getSalary() != null && !updatedEmployee.getSalary().equals(0)) {
       employee.setSalary(updatedEmployee.getSalary());
     }
-    return employeeRepository.save(id, employee);
+    return employeeRepositoryInMongo.save(employee);
+//    return employeeRepository.save(id, employee);
   }
 
   public void delete(String id) {
-    employeeRepository.delete(id);
+    employeeRepositoryInMongo.delete(findById(id));
+//    employeeRepository.delete(id);
   }
 
-  public List<Employee> findByCompanyId(String id) {
-    return employeeRepository.findByCompanyId(id);
+  public List<Employee> findByCompanyId(String companyId) {
+    return employeeRepositoryInMongo.findByCompanyId(companyId);
+//    return employeeRepository.findByCompanyId(id);
   }
 }
